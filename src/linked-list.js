c7d33a11eff607a4217043cc5dd9b91e75fd26cf
ReaziS <kost9n98@yandex.ru>
2019-10-07 14:18:22 +0300

    feat: passed half tests

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,15 +1,65 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor(head = null, tail = null, length = 0) {
+        this._head = head;
+        this._tail = tail;
+        this.length = length;
+    }
 
-    append(data) {}
+    append(data) {
+        
+        if(!this._head){
+            let node = new Node(data);
 
-    head() {}
+            this._head = node;
+            this._tail = node;
+            this.length++;
+        } else {
+            let node = new Node(data, this._tail, this._head );
+            this._tail = node;
+            this.length++;
+        }
+        return this;
+    }
+    
 
-    tail() {}
+    clear() {
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
+        return this;
+    }
+    head() { return this.length === 0 ? null : this._head.data ;}
 
-    at(index) {}
+    tail() { return this.length === 0 ? null : this._tail.data; }
+
+    isEmpty() {
+        return this.length === 0 ? true : false;
+    }
+    at(index) {
+        let currentNode = this._head;
+        console.log(currentNode.prev);
+        for(let i = 0; i < index; i++){
+        }
+        return currentNode.data;
+    }
+}
+
+let list = new LinkedList;
+list.append(1);
+list.append(123);
+list.append(444);
+list.append(3);
+console.log(list.at(2));
+/*
+    at(index) {
+        let currentNode = this.headNode;
+        for(let i = 0; i < index; i++){
+            currentNode = currentNode.next;
+        }
+        return currentNode.data;
+    }
 
     insertAt(index, data) {}
 
@@ -23,5 +73,5 @@ class LinkedList {
 
     indexOf(data) {}
 }
-
+*/
 module.exports = LinkedList;
